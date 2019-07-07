5018bb5db3d597a3341333c9e4d792eae4c1221e
Miada Roshdy <miadaroshdy@Miadas-MacBook-Air.local>
2019-07-07 12:02:07 -0400

    Add lane changing and trial files

--- src/main_1.cpp
+++ src/main_1.cpp
@@ -7,7 +7,7 @@
 #include "Eigen-3.3/Eigen/QR"
 #include "helpers.h"
 #include "json.hpp"
-#inclue "spline.h"
+#include "spline.h"
 
 // for convenience
 using nlohmann::json;
@@ -27,7 +27,7 @@ int main() {
   // Waypoint map to read from
   string map_file_ = "../data/highway_map.csv";
   // The max s value before wrapping around the track back to 0
-  double max_s = 6945.554;
+  double max_s = 6945.554; // track length
 
   std::ifstream in_map_(map_file_.c_str(), std::ifstream::in);
 
@@ -54,12 +54,10 @@ int main() {
     int lane = 1;
     
     // define reference velocity as car target velocity
-    double ref_vel = 49.5; // close to speed limit which is 50-mph
+    double ref_vel = 0.0; // To make car start reach gradually to target velocity
 
-  h.onMessage([&map_waypoints_x,&map_waypoints_y,&map_waypoints_s,
-               &map_waypoints_dx,&map_waypoints_dy]
-              (uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
-               uWS::OpCode opCode) {
+  h.onMessage([&map_waypoints_x,&map_waypoints_y,&map_waypoints_s,&map_waypoints_dx,&map_waypoints_dy,&lane,&ref_vel](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
+                     uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
     // The 4 signifies a websocket message
     // The 2 signifies a websocket event
@@ -96,6 +94,90 @@ int main() {
             
           // get previous path data points size
           int prev_size = previous_path_x.size();
+
+
+			if (prev_size > 0) {
+				car_s = end_path_s; // change car's s to reflect previous path last point s
+			}
+
+			// Lane identifiers for other cars
+			bool too_close = false;
+			bool car_left = false;
+			bool car_right = false;
+
+			// Find ref_v to use, see if car is in lane
+			for (int i = 0; i < sensor_fusion.size(); i++) {
+				// Car is in my lane
+				float d = sensor_fusion[i][6];
+
+				// Identify the lane of the car in question
+				int car_lane;
+				if (d >= 0 && d < 4) {
+					car_lane = 0;
+				} else if (d >= 4 && d < 8) {
+					car_lane = 1;
+				} else if (d >= 8 && d <= 12) {
+					car_lane = 2;
+				} else {
+					continue;
+				}
+
+				// Check width of lane, in case cars are merging into our lane
+				double vx = sensor_fusion[i][3];
+				double vy = sensor_fusion[i][4];
+				double check_speed = sqrt(vx*vx + vy*vy);
+				double check_car_s = sensor_fusion[i][5];
+
+				// If using previous points can project an s value outwards in time
+				// (What position we will be in in the future)
+				// check s values greater than ours and s gap
+				check_car_s += ((double)prev_size*0.02*check_speed);
+
+				int gap = 30; // m
+
+				// Identify whether the car is ahead, to the left, or to the right
+				if (car_lane == lane) {
+					// Another car is ahead
+					too_close |= (check_car_s > car_s) && ((check_car_s - car_s) < gap);
+				} else if (car_lane - lane == 1) {
+					// Another car is to the right
+					car_right |= ((car_s - gap) < check_car_s) && ((car_s + gap) > check_car_s);
+				} else if (lane - car_lane == 1) {
+					// Another car is to the left
+					car_left |= ((car_s - gap) < check_car_s) && ((car_s + gap) > check_car_s);
+				}
+			}
+
+			// Modulate the speed to avoid collisions. Change lanes if it is safe to do so (nobody to the side)
+			double acc = 0.224; // acceleration/decelaration step
+			double max_speed = 49.5;
+			if (too_close) {
+				// A car is ahead
+				// Decide to shift lanes or slow down
+				if (!car_right && lane < 2) {
+					// No car to the right AND there is a right lane -> shift right
+					lane++;
+				} else if (!car_left && lane > 0) {
+					// No car to the left AND there is a left lane -> shift left
+					lane--;
+				} else {
+					// Nowhere to shift -> slow down
+					ref_vel -= acc;
+				}
+			} else {
+				if (lane != 1) {
+					// Not in the center lane. Check if it is safe to move back
+					if ((lane == 2 && !car_left) || (lane == 0 && !car_right)) {
+						// Move back to the center lane
+						lane = 1;
+					}
+				}
+
+				if (ref_vel < max_speed) {
+					// No car ahead AND we are below the speed limit -> speed limit
+					ref_vel += acc;
+				}
+			}
           
           // create a list of (x,y) waypoints to spline interpolate them.
             vector<double> ptsx;
@@ -146,7 +228,7 @@ int main() {
            // Add 3 more points , where each is 30m spaced using the passed middle laneand map waypoints
              vector<double> next_wp0 = getXY(car_s+30, (6*lane), map_waypoints_s, map_waypoints_x, map_waypoints_y);
              vector<double> next_wp1 = getXY(car_s+60, (6*lane), map_waypoints_s, map_waypoints_x, map_waypoints_y);
-             vector<double> next_wp2 = getXY(car_s+90, (6*lane), map_waypoints_s, map_waypoints_x, map_waypoints_y);
+             vector<double> next_wp2 = getXY(car_s+90, (2+4*lane), map_waypoints_s, map_waypoints_x, map_waypoints_y);
             
             ptsx.push_back(next_wp0[0]);
             ptsx.push_back(next_wp1[0]);
@@ -192,10 +274,36 @@ int main() {
                 next_x_vals.push_back(previous_path_x[i]);
                 next_y_vals.push_back(previous_path_y[i]);
             }
-            
-            
-            msgJson["next_x"] = next_x_vals;
-            msgJson["next_y"] = next_y_vals;
+
+            // Calculate how to break up spline points so we travel at our desired reference velocity
+            // this part is to get the y-values of the x-values points for spline using triangle math
+			double target_x = 30.0; //path horizon
+			double target_y = s(target_x);
+			double target_dist = sqrt((target_x) * (target_x) + (target_y) * (target_y));
+			double x_add_on = 0; // start at the origin of car co-ordinates (local co-ordinates)
+
+			// Fill up the rest of the path planner to always output 50 points
+			for (int i = 1; i <= 50 - previous_path_x.size(); i++) {
+				double N = (target_dist/(.02*ref_vel/2.24)); // number of points on the spline path
+				double x_point = x_add_on + (target_x) / N;
+				double y_point = s(x_point);
+
+				x_add_on = x_point;
+
+				double x_ref = x_point;
+				double y_ref = y_point;
+
+				// Rotate back to normal after rotating it earlier (i.e. go back to global co-ordinates
+				x_point = (x_ref * cos(ref_yaw) - y_ref*sin(ref_yaw));
+				y_point = (x_ref * sin(ref_yaw) + y_ref*cos(ref_yaw));
+
+				x_point += ref_x;
+				y_point += ref_y;
+
+				next_x_vals.push_back(x_point);
+				next_y_vals.push_back(y_point);
+			}
+
 
 
           msgJson["next_x"] = next_x_vals;
